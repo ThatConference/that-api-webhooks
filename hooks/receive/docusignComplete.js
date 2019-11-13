aa0ede81bd15b1788fac583cd8ccedc8e1e291ae
Brett Slaski <brettski@yahoo.com>
2019-11-13 13:30:34 -0600

    Add eslint linting fix issue with no template match

--- hooks/receive/docusignComplete.js
+++ hooks/receive/docusignComplete.js
@@ -1,71 +1,73 @@
-'use strict'
-
 // Catch completed docusign webhook
-//import axios from 'axios'
-const axios = require('axios')
-const Sentry = require('@sentry/node')
-const {parseDocuSignXml} = require('../../parsers/docuSign')
+const axios = require('axios');
+const Sentry = require('@sentry/node');
+const { parseDocuSignXml } = require('../../parsers/docuSign');
 
-Sentry.init({ dsn: process.env.SENTRY_NODE_DSN })
+Sentry.init({ dsn: process.env.SENTRY_NODE_DSN });
 
 module.exports = (req, res) => {
-    console.log('trigger docusignComplete')
-    let payload = []
-    if (req.method == 'POST') {
-        console.log('post')
-        if(req.body) {
-            usePayload(req.body, res)
-        } 
-        else {        
-            req
-                .on('error', err => {
-                    console.error(err)
-                })
-                .on('data', chunk => {
-                    console.log('data event')
-                    payload.push(chunk);
-                })
-                .on('end', () => {
-                    console.log('end event')
-                    payload = Buffer.concat(payload).toString()
-                    //console.log('payload received:', payload)
-                    //console.log('end of payload')
-                    usePayload(payload, res)
-                })
-        }
+  console.log('trigger docusignComplete');
+  let payload = [];
+  if (req.method === 'POST') {
+    console.log('post');
+    if (req.body) {
+      usePayload(req.body, res);
     } else {
-        console.error(`Non POST request, ${req.method}`)
-        Sentry.captureMessage(`Non POST request, ${req.method}`, 'warning' )
-        res.writeHead(405, {'Content-Type': 'text/plain'})
-        res.write(`What was that? I didn't like that much.`)
-        res.end()
+      req
+        .on('error', err => {
+          console.error(err);
+        })
+        .on('data', chunk => {
+          console.log('data event');
+          payload.push(chunk);
+        })
+        .on('end', () => {
+          console.log('end event');
+          payload = Buffer.concat(payload).toString();
+          usePayload(payload, res);
+        });
     }
-}
+  } else {
+    console.error(`Non POST request, ${req.method}`);
+    Sentry.captureMessage(`Non POST request, ${req.method}`, 'warning');
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
+    res.write(`What was that? I didn't like that much.`);
+    res.end();
+  }
+};
 
 const usePayload = (payload, res) => {
-    parseDocuSignXml(payload)
-        .then(docusign => {
-            axios.post(process.env.ZAP_DOCUSIGN_IN_HOOK_URL, docusign)
-                .then( axresp => {
-                    console.log(`axios res status ${axresp.status}`)
-                    if (axresp.status < 200 || axresp.status > 299) {
-                        console.error(`Issue sending hook. status ${axresp.status}`)
-                        Sentry.captureMessage(`Issue sending webhook request: ${axresp}`)
-                        return
-                    }    
-                    res.writeHead(200, {'Content-Type': 'application/json'})
-                    res.write(JSON.stringify(docusign))
-                    res.end()
-                }).catch(err => {
-                    console.error(err)
-                    Sentry.captureException(err)
-                    res.writeHead(500, {'Content-Type':'text/plain'})
-                    res.write(err.message)
-                    res.end()
-                    return
-                })
-        }).catch(err => {
-            console.error(err)
-            res.end()
+  parseDocuSignXml(payload)
+    .then(docusign => {
+      if (!docusign) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write('Unknown DocuSign template, skipping. See log for details');
+        res.end();
+        return;
+      }
+      axios
+        .post(process.env.ZAP_DOCUSIGN_IN_HOOK_URL, docusign)
+        .then(axresp => {
+          console.log(`axios res status ${axresp.status}`);
+          if (axresp.status < 200 || axresp.status > 299) {
+            console.error(`Issue sending hook. status ${axresp.status}`);
+            Sentry.captureMessage(`Issue sending webhook request: ${axresp}`);
+            return;
+          }
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.write(JSON.stringify(docusign));
+          res.end();
+        })
+        .catch(err => {
+          console.error(err);
+          Sentry.captureException(err);
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.write(err.message);
+          res.end();
+        });
     })
-}
+    .catch(err => {
+      console.error(err);
+      res.end();
+    });
+};
--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-'use strict'
+const dotenv = require('dotenv');
 
-const dotenv = require('dotenv')
-dotenv.config()
-const express = require('express')
-const {auth} = require('./mw/auth')
-const docusignComplete = require('./hooks/receive/docusignComplete')
+dotenv.config();
+const express = require('express');
+const { auth } = require('./mw/auth');
+const docusignComplete = require('./hooks/receive/docusignComplete');
 
+const app = express();
+app.use(auth);
+app.use('/docusignComplete', docusignComplete);
 
-const app = express()
-app.use(auth)
-app.use('/docusignComplete', docusignComplete)
-
-exports.hooks = app
-
+exports.hooks = app;
